d56f69ac1d492efaae81f1395d2edb245c4b43aa
kukuru5a <romanzag@icloud.com>
2024-06-01 00:38:17 +0600

    Upds

--- src/main/java/org/example/vkBot/controller/BotController.java
+++ src/main/java/org/example/vkBot/controller/BotController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.*;
 import java.io.IOException;
 import java.util.Map;
 
-import static org.example.vkBot.config.Config.CONFIRMATION_TOKEN;
 
 @RestController
 @RequestMapping
